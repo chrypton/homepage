a65d675d56f045798d4605d85318147f6303dde3
chrypton <chryptonium@gmail.com>
2024-04-23 08:38:21 -0400

    Included dayjs duration plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
+import duration from 'dayjs/plugin/duration';
 
 window.axios = axios;
 
@@ -10,3 +11,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.dayjs = dayjs;
 dayjs.extend(relativeTime);
 dayjs.extend(advancedFormat);
+dayjs.extend(duration);
