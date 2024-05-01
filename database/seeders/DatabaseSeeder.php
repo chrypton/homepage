9b60002403de247c805f88149670076082dd4826
chrypton <chryptonium@gmail.com>
2024-05-01 02:22:49 -0400

    Removed dummy data in prep for actual seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Bookmark;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,19 +13,6 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run(): void
 	{
-		User::create([
-			'name' => 'Admin',
-			'username' => 'admin',
-			'email' => 'admin@homepage.test',
-			'password' => Hash::make('adminpassword'),
-		]);
-
-		User::factory(5)->create();
-
-		foreach (User::pluck('id') as $id) {
-			for ($j=0; $j < 20; $j++) { 
-				Bookmark::factory()->create(['user_id' => $id]);
-			}
-		}
+		// 
 	}
 }
