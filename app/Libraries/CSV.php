18fa71ceea91dbbca7e597c24a8608c68f43464c
chrypton <chryptonium@gmail.com>
2024-05-01 07:12:58 -0400

    Created basic CSV handler

---
+++ app/Libraries/CSV.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Libraries;
+
+use Illuminate\Support\Collection;
+
+class CSV {
+	private Collection $headers;
+	public Collection $data;
+
+	function __construct(string $filepath)
+	{
+		$this->data = collect(array_map('str_getcsv', file(base_path("storage\\csv\\{$filepath}"))));
+		$this->headers = collect($this->data->shift());
+		
+		$this->data->transform(function ($csvRow){
+			return $this->headers->combine($csvRow);
+		});
+	}
+}
